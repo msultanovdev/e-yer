77f5ba21aa4d69c1f66363264a93460a0318f57c
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-06-09 23:28:46 +0500

    Adding modal popup

--- components/e-nazorat modal/index.js
+++
@@ -1,7 +0,0 @@
-ymaps.ready(init);
-        function init(){
-            var myMap = new ymaps.Map("map", {
-                center: [41.311081, 69.240562],
-                zoom: 7
-            });
-        }
